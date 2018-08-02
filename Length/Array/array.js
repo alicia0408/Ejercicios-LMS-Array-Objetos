dbe7f62b0fc71739f0b26fe8de550bd272d307c1
Alicia Centeno <alicent.08.02@gmail.com>
2018-08-02 18:40:58 -0400

    funcion areValidCredentials

--- Length/Array/array.js
+++ Length/Array/array.js
@@ -1,3 +1,15 @@
 function getNthElement(array, n) {
 	return array[n];
 }
+
+function areValidCredentials(nombre, contraseña) {
+	let nombre = nombre.length;
+	let contraseña = contraseña.length;
+
+	if (nombre > 3 && contraseña >= 8) {
+		return true 
+	} else {
+		return false
+	} 
+
+}
