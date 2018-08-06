13b919d85b1297bd3a2b624088e97f334b06fe52
Alicia Centeno <alicent.08.02@gmail.com>
2018-08-06 11:39:06 -0400

    getSumOfAllElementsAtProperty

---
+++ Length/objetos/objetos.js
@@ -0,0 +1,50 @@
+function getElementOfArrayProperty(obj, key, index) {
+
+	if (obj[key] === undefined) {
+		return undefined;
+	}
+	if (Array.isArray(obj[key]) === false) {
+		return undefined;
+	}
+
+	return obj[key][index];
+}
+
+function addObjectProperty(obj1, key, obj2) {
+obj1[key] = obj2; 
+	return obj1;
+}
+
+function getAllKeys(obj) {
+    let array = [];
+	for (let key in obj) {
+		array.push(key);
+	}
+	return array;
+
+}
+
+function getSumOfAllElementsAtProperty(obj, key) {
+	let array = obj[[key]]
+	if (key === undefined) {
+		return 0
+	}
+	else if (Array.isArray(array) === false) {
+		return 0
+	}
+	else if (array.length === 0) {
+		return 0
+	}
+
+	else {
+		let sum = 0
+		for (var i = 0; i < array.length; i++) {
+			sum += array[i]
+		}
+	}
+	return sum
+
+
+
+
+}
